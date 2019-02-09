442ce8cb67b6e142b4dab01f3f0b63bf68b805be
Alabhya Pandey <alabhyap@gmail.com>
2019-02-10 03:31:32 +0530

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { RouteGaurdService } from './route-gaurd.service';
 const routes: Routes = [
   {path:'login',component:LoginComponent,canActivate:[LoginRouteGaurdService]},
   {path:'dashboard',component:DashboardComponent,canActivate:[RouteGaurdService]},
-  {path:'/',redirectTo:'login',pathMatch:'full'},
+  {path:'',redirectTo:'login',pathMatch:'full'},
   {path:'*',redirectTo:'login',pathMatch:'full'},
   {path:'**',redirectTo:'login',pathMatch:'full'}
 ];
